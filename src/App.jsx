40f7f92f1656c6b6b87da9cbbc4be94798258117
Ravi Kumar <ravikumar.developerbabu@gmail.com>
2023-12-28 23:49:33 +0530

    us list down the dynamic components using dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { render } from 'react-dom'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {ENVIRONMENT_DATA} from  "./data/testing_data.js"
 
-const rendomWorkds = ['Development', 'Testing', 'Production']
+const rendomWorkds = ["Development", "Testing", "Production"];
 
 const getRandomNumber = (max) => {
-  return Math.floor(Math.random() * (max));
-}
+  return Math.floor(Math.random() * max);
+};
 
-function DetialsOfEnvironment(props){
-  return(<li>
-    <h2>{props.title}</h2>
-    <p>{props.description}</p>
-  </li>)
+function DetialsOfEnvironment(props) {
+  return (
+    <li>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  const environment = rendomWorkds[getRandomNumber(rendomWorkds.length)]
+  const environment = rendomWorkds[getRandomNumber(rendomWorkds.length)];
   return (
     <>
       <div>
@@ -41,16 +43,31 @@ function App() {
         </p>
         <p>The Application is running in {environment} Environment.</p>
         <ul>
-          <DetialsOfEnvironment title='Testing' description="it is used to test the application with test cases."/>
-          <DetialsOfEnvironment title='Production' description="it is used where application is live and used by clients or real users."/>
-          <DetialsOfEnvironment title='Development' description="it is used to build the application by the developer."/>
+          {ENVIRONMENT_DATA.map((data)=>{
+            return (<DetialsOfEnvironment
+            title={data.title}
+            description={data.description}
+          />)
+          })}
+          {/* <DetialsOfEnvironment
+            title="Testing"
+            description="it is used to test the application with test cases."
+          />
+          <DetialsOfEnvironment
+            title="Production"
+            description="it is used where application is live and used by clients or real users."
+          />
+          <DetialsOfEnvironment
+            title="Development"
+            description="it is used to build the application by the developer."
+          /> */}
         </ul>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/testing_data.js
@@ -0,0 +1,15 @@
+export const ENVIRONMENT_DATA = [
+  {
+    title: "Testing",
+    description: "it is used to test the application with test cases.",
+  },
+  {
+    title: "Production",
+    description:
+      "it is used where application is live and used by clients or real users.",
+  },
+  {
+    title: "Development",
+    description: "it is used to build the application by the developer.",
+  },
+];
