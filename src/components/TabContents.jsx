5d570533d03332ca371242ddfcdd665692461005
Ravi Kumar <ravikumar.developerbabu@gmail.com>
2024-01-04 20:23:17 +0530

    dynamic section in Reactjs

--- src/components/TabContents.jsx
+++ src/components/TabContents.jsx
@@ -24,6 +24,7 @@ export default function TabContents() {
   return (
     <Section id="example" title="TabButton Section">
       <Tabs
+        buttonsContainer="menu"
         buttons={
           <>
             <TabButton
